f858144616ab402c35b44afae88979ec403b78cc
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-03-03 12:36:53 +0000

    variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+const accountId= 144663
+let accountEmail = "work.aaryanbudakoti@gmail.com"
+var accountPassword = "12345"
+accountCity="Jaipur"
+
+console.log(accountId);
+
+accountEmail="abc@gmail.com"
+accountPassword ="123313"
+accountCity="ghaziabad"
+console.log()
+
+console.table([accountId,accountEmail, accountPassword, accountCity])
+
