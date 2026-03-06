878b3baec7c4098ac70c00a57b0883455acbd6bd
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-03-06 11:44:36 +0000

    conversion and operations

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -1,29 +1,74 @@
-let score = "aaryan"
+// let score = "aaryan"
 
-console.log(typeof (score))
+// console.log(typeof (score))
 
-// conversion of string to number
+// // conversion of string to number
 
-let valueInNumber = Number(score) // guarantees the input to be a number, in case it is input as a string
+// let valueInNumber = Number(score) // guarantees the input to be a number, in case it is input as a string
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber) //NaN-  not a number
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //NaN-  not a number
 
-// conversion of .. number - number, "33abc"- number , true - 1, false - 0, null - number, undefined - NaN
+// // conversion of .. number - number, "33abc"- number , true - 1, false - 0, null - number, undefined - NaN
 
-let isLoggedIn= "abc"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedIn= "abc"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-// 1- true, 0 - false
-// " " -empty - 0, "aaryan "- non-empty - 1
+// // 1- true, 0 - false
+// // " " -empty - 0, "aaryan "- non-empty - 1
 
-let sum = 33
-let stringSum= String(sum)
+// let sum = 33
+// let stringSum= String(sum)
 
-console.log(typeof stringSum)
-console.log(stringSum)
+// console.log(typeof stringSum)
+// console.log(stringSum)
 
 
+// OPERATIONS
+
+// let value = 3
+// let negValue= -value 
+// // console.log(negValue)
+
+// //basic math operations can be printed
+
+// // let str1= "Hello"
+// // let str2= " Aaryan"
+
+// // let str3 = str1 + str2
+// // console.log(str3);
+
+// // console.log("1"+2 + 2); //122
+// // console.log(1+2+"2"); //32
+
+// console.log(+true) //1
+// console.log(+"") // 0
+
+
+// let num1, num2, num3
+
+// num1=num2=num3 = 2+2
+
+// console.log(num2);
+
+// let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter);
+
+
+// Comparisons
+// console.log(2>1)
+// console.log(2>=4)
+
+console.log("02">1) // automatic conversion to integer 02 - true
+console.log(null==0) 
+console.log(null>=0)
+console.log(null>0)
+
+console.log(null!=0)
+
+
+//=== strixt check - checks datatype and value both
