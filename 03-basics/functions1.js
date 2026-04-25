9e0a34bd63558e88f2f0406d02f77b1006eb655c
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-04-25 13:26:07 +0000

    Add functions for shopping cart logic and return second value from array

--- 03-basics/functions1.js
+++ 03-basics/functions1.js
@@ -46,6 +46,39 @@ function loginUserMessage(username = "user"){ // this is how we can set a defaul
     return ` ${username} just logged in`
 }
 
-console.log(loginUserMessage("Aaryan")); // this is how we can call a function with arguments and it will return the message in the console
-console.log(loginUserMessage(   )); // this is how we can call a function with arguments and it will return the message in the console, but since we have not passed any argument, it will return "undefined just logged in" because username is undefined in this case
+// console.log(loginUserMessage("Aaryan")); // this is how we can call a function with arguments and it will return the message in the console
+// console.log(loginUserMessage(   )); // this is how we can call a function with arguments and it will return the message in the console, but since we have not passed any argument, it will return "undefined just logged in" because username is undefined in this case
 
+// shopping cart logic (example of a function with multiple parameters and default values)
+
+function calculateCartPrice (...num1){ //rest operator is used to take all the arguments passed to the function and store them in an array, so num1 will be an array containing all the arguments passed to the function
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500)); // this will return an array containing all the arguments passed to the function
+
+
+const User = {
+    username: "aaryan",
+    price : 500,
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(User); 
+
+handleObject({
+    username: "aaryan",
+    price : 500,
+}); // this is how we can pass an object as an argument to a function without storing it in a variable, this is called an anonymous object
+
+
+const myarr = [200,1000,400,300]
+
+function returnSecondValue(getarray){
+    return getarray[1]; // this will return the second value of the array   
+}
+
+console.log(returnSecondValue(myarr)); // this will return the second value of the array which is 1000
