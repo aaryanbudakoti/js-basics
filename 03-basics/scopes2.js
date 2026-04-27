b0e0dac9ec931a07601e8cc7a21fa3d9533dd654
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-04-27 12:19:21 +0000

    add explanations for function hoisting behavior

--- 03-basics/scopes2.js
+++ 03-basics/scopes2.js
@@ -25,7 +25,7 @@ function one() {
 
     function two(){
         const website = "www.aaryan.com"
-        console.log(username); // username is accessible in function two because it is defined in the outer scope, so it is accessible in the inner scope
+        // console.log(username); // username is accessible in function two because it is defined in the outer scope, so it is accessible in the inner scope
     }
     // console.log(website); // website is not accessible in function one because it is defined in the inner scope, so it is not accessible in the outer scope
     
@@ -38,7 +38,22 @@ one()
 
 if (true) {
     let d = 400
-    console.log(d); // d is accessible within the block because it is defined in the block scope, but it is not accessible outside the block because it is not defined in the global scope
+    // console.log(d); // d is accessible within the block because it is defined in the block scope, but it is not accessible outside the block because it is not defined in the global scope
 }
 
 // console.log(d); // d is not accessible outside the block because it is defined in the block scope, so it is not accessible in the global scope
+
+
+// ******************************************* interesting concept *******************************************
+ addone(5) // this will work because addone is a function declaration and it is hoisted, so it is accessible before it is defined
+function addone(num){
+    return num + 1;
+}
+
+
+addtwo(5) // this will throw an error because addtwo is not defined before it is called, because it is a function expression and it is not hoisted, so it is not accessible before it is defined
+const addtwo = function(num){
+    return num + 2;
+}
+// addtwo is a function expression, it is not hoisted, so it is not accessible before it is defined, so it will throw an error if we try to call it before it is defined
+
