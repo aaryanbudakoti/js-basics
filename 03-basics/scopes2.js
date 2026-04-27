da42013b32e394ba0d2ce12d033e2f3ae6874747
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-04-27 12:11:26 +0000

    Comment out console.log statements to enhance code clarity and prevent unnecessary output

--- 03-basics/scopes2.js
+++ 03-basics/scopes2.js
@@ -5,7 +5,7 @@ if (true){
     let a = 10
     const b = 20 
      let c = 30
-     console.log(c);
+    //  console.log(c);
      
  // block scope- it is only accessible within the block it is defined in, so a and b are not accessible outside the block, but c is accessible outside the block because it is defined in the global scope
     } 
@@ -16,5 +16,29 @@ if (true){
 
 // console.log(a);
 // console.log(b);
-console.log(c); // var is function scoped, so it is accessible outside the block, but let and const are block scoped, so they are not accessible outside the block
+// console.log(c); // var is function scoped, so it is accessible outside the block, but let and const are block scoped, so they are not accessible outside the block
 
+
+
+function one() {
+    const username = "aaryan"
+
+    function two(){
+        const website = "www.aaryan.com"
+        console.log(username); // username is accessible in function two because it is defined in the outer scope, so it is accessible in the inner scope
+    }
+    // console.log(website); // website is not accessible in function one because it is defined in the inner scope, so it is not accessible in the outer scope
+    
+    two()
+    
+}
+one()
+
+// child function can access the variables of the parent function, but vice versa is not true
+
+if (true) {
+    let d = 400
+    console.log(d); // d is accessible within the block because it is defined in the block scope, but it is not accessible outside the block because it is not defined in the global scope
+}
+
+// console.log(d); // d is not accessible outside the block because it is defined in the block scope, so it is not accessible in the global scope
