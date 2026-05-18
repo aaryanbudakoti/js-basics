3167a6a93d649e59d6cf888935580a602cf99849
Aaryan Budakoti <work.aaryanbudakoti@gmail.com>
2026-05-18 13:48:20 +0000

    add example demonstrating array manipulation using map and filter methods

---
+++ 05-iterations/fmr.js
@@ -0,0 +1,17 @@
+//
+
+const myNums = [1, 2, 3, 4, 5,6,7,8,9,10]
+
+// map method creates a new array with the results of calling a provided function on every element in the calling array
+// const greaterNums = myNums.map((num) =>  num +10 )
+// console.log(greaterNums);
+
+
+//chaining map and filter methods
+// chaining is a technique where we can call multiple methods on the same array, it allows us to perform multiple operations on the same array without having to create intermediate variables.
+const newNums = myNums
+                .map((num) => num * 10) // first we multiply each number by 10 // output will be [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+                .map((num) => num + 10) // then we add 10 to each number // output will be [20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
+                .filter((num) => num > 50) // then we filter the numbers that are greater than 50 // output will be [60, 70, 80, 90, 100, 110]
+
+console.log(newNums);
